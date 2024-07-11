8854d23ea2f252e7e9dd0d24f184d124651216c6
Hamster5295 <hamster5295@163.com>
2024-07-11 11:22:12 +0800

    增加外设类型别名

--- Inc/hkit.h
+++ Inc/hkit.h
@@ -14,11 +14,17 @@
 #define f32   float
 #define f64   double
 
-#define bool u8
+#define bool  u8
 
 #define true  1
 #define false 0
 #define null  0
+
+// HAL 库的数据类型
+#define STATUS HAL_StatusTypeDef
+
+#define UART   UART_HandleTypeDef
+#define I2C    I2C_HandleTypeDef
 
 // 这个 include 是为了找到当前的 hal 库
 // 从而不需要手动设置 stm32 型号
@@ -59,7 +65,7 @@ void HKIT_TriggerError(HError err);
  * @param err 错误代码
  * @return 存放错误信息的字符串
  */
-u8* HKIT_ErrorToString(HError err);
+u8 *HKIT_ErrorToString(HError err);
 
 /**
  * 默认错误处理器，会将错误转为字符串后输出
