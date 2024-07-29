71e312122f45fe4d500f67d2005700c03cc781ee
Hamster5295 <hamster5295@163.com>
2024-07-29 22:53:51 +0800

    增加舍入宏

--- Inc/hmath.h
+++ Inc/hmath.h
@@ -1,9 +1,15 @@
 #if !defined(__HMATH_H__)
 #define __HMATH_H__
 
+#include "hkit.h"
+
 #define HMAX(x, y)          (((x) > (y)) ? (x) : (y))
 #define HMIN(x, y)          (((x) > (y)) ? (y) : (x))
 #define HCLAMP(x, max, min) (((x) > (max)) ? (max) : (((x) < (min)) ? (min) : (x)))
+
+#define HFLOOR(x)           ((i32)(x))
+#define HCEIL(x)            ((i32)(x + 0.999f))
+#define HROUND(x)           ((i32)(x + 0.5f))
 
 #define HDIFF_ABS(x, y)     (((x) > (y)) ? ((x) - (y)) : ((y) - (x)))
 
